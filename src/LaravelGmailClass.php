23bdee588227b59db00179ef7ff8f2a83ba9c238
pamekar <d.pamekar@gmail.com>
2020-09-10 12:35:06 +0100

    Set access Token for message request
    Signed-off-by: pamekar <d.pamekar@gmail.com>

--- src/LaravelGmailClass.php
+++ src/LaravelGmailClass.php
@@ -8,66 +8,69 @@ use Illuminate\Support\Facades\Redirect;
 
 class LaravelGmailClass extends GmailConnection
 {
-	public function __construct($config, $userId = null)
-	{
-		if (class_basename($config) === 'Application') {
-			$config = $config['config'];
-		}
+    public function __construct($config, $userId = null)
+    {
+        if (class_basename($config) === 'Application') {
+            $config = $config['config'];
+        }
 
-		parent::__construct($config, $userId);
-	}
+        parent::__construct($config, $userId);
+    }
 
-	/**
-	 * @return Message
-	 * @throws AuthException
-	 */
-	public function message()
-	{
-		if (!$this->getToken()) {
-			throw new AuthException('No credentials found.');
-		}
+    /**
+     * @return Message
+     * @throws AuthException
+     */
+    public function message($token = null)
+    {
+        if ($token) {
+            $this->setAccessToken($token);
+        }
+        if (!$this->getToken()) {
+            throw new AuthException('No credentials found.');
+        }
 
-		return new Message($this);
-	}
+        return new Message($this);
+    }
 
-	/**
-	 * Returns the Gmail user email
-	 *
-	 * @return \Google_Service_Gmail_Profile
-	 */
-	public function user()
-	{
-		return $this->config('email');
-	}
+    /**
+     * Returns the Gmail user email
+     *
+     * @return \Google_Service_Gmail_Profile
+     */
+    public function user()
+    {
+        return $this->config('email');
+    }
 
-	/**
-	 * Updates / sets the current userId for the service
-	 *
-	 * @return \Google_Service_Gmail_Profile
-	 */
-	public function setUserId($userId)
-	{
-		$this->userId = $userId;
-		return $this;
-	}
+    /**
+     * Updates / sets the current userId for the service
+     *
+     * @return \Google_Service_Gmail_Profile
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+        return $this;
+    }
 
-	public function redirect()
-	{
-		return Redirect::to($this->getAuthUrl());
-	}
+    public function redirect()
+    {
+        return Redirect::to($this->getAuthUrl());
+    }
 
-	/**
-	 * Gets the URL to authorize the user
-	 *
-	 * @return string
-	 */
-	public function getAuthUrl()
-	{
-		return $this->createAuthUrl();
-	}
+    /**
+     * Gets the URL to authorize the user
+     *
+     * @return string
+     */
+    public function getAuthUrl()
+    {
+        return $this->createAuthUrl();
+    }
 
-	public function logout()
-	{
+    public function logout()
+    {
         $this->stop('me');
         $this->revokeToken();
         $this->deleteAccessToken();
